c93224b058cbba6c173f0b7c6d86522f5015d32f
Abid-Chowdhury <mr.abid2004@gmail.com>
2023-08-13 01:52:13 -0400

    Added search page

--- src/home_page/script.js
+++ src/home_page/script.js
@@ -3,8 +3,13 @@ function get(str) {
 }
 
 // search button function
+function redirectToSearchPage() {
+    location.href = '../search_page/search.html'
+}
+
 var searchIcon = get('search-icon')
 searchIcon.onclick = function () {
-    var searchQuery = get('search-query')
-    console.log(searchQuery.value)
+    // var searchQuery = get('search-query')
+    // console.log(searchQuery.value)
+    redirectToSearchPage()
 }
