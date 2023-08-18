a6ab09d1f04177612dff87b8b4bd9557cb2bb89b
Abid-Chowdhury <mr.abid2004@gmail.com>
2023-08-18 19:24:52 -0400

    Added proxy server

---
+++ src/proxy-server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const https = require('https');
+const url = require('url');
+
+// Target API URL
+const targetUrl = 'https://api.example.com'; // Replace with your API's URL
+
+// Create the proxy server
+const proxyServer = http.createServer((req, res) => {
+  const target = url.parse(targetUrl + req.url);
+  const options = {
+    host: target.hostname,
+    port: target.port || (target.protocol === 'https:' ? 443 : 80),
+    path: target.path,
+    method: req.method,
+    headers: req.headers,
+  };
+
+  const proxyRequest = (target.protocol === 'https:' ? https : http).request(options, (proxyResponse) => {
+    res.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+    proxyResponse.pipe(res, {
+      end: true
+    });
+  });
+
+  req.pipe(proxyRequest, {
+    end: true
+  });
+});
+
+// Start the proxy server on port 3000
+const PORT = 3000;
+proxyServer.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${PORT}`);
+});
--- src/search_page/search.js
+++ src/search_page/search.js
@@ -7,6 +7,18 @@ var searchQuery = (window.location.search).replace('?', '')
 var searchInput = document.getElementById('search-query')
 searchInput.value = searchQuery
 
+// api call
+const apiUrl = `http://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+
 // update total results
 var totalResults = getID('total-results')
 totalResults.innerHTML = `12 results for ${searchQuery}`
