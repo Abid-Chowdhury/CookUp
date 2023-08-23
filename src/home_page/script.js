df7a8052bae64c3f1296e672130cf6525a8fe9c0
Abid-Chowdhury <mr.abid2004@gmail.com>
2023-08-22 20:18:58 -0400

    Updated all recipes

--- src/home_page/script.js
+++ src/home_page/script.js
@@ -19,3 +19,75 @@ sq.addEventListener('keydown', (e) => {
         redirectToSearchPage()
     }
 });
+
+// api call
+async function fetchAPI() {
+    const apiUrl = `http://www.themealdb.com/api/json/v1/1/search.php?s=`
+    
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        // console.log(data)
+        return data
+    } catch (error) {
+        // console.error('Fetch error:', error)
+        return 0
+    }
+}
+
+fetchAPI()
+    .then(data => {
+        results = data.meals.length
+        meals = data.meals
+        showResults(results, meals)
+    })
+
+function showResults(results, meals) {
+    const resultsContainer = document.getElementById('results-container')
+
+    for (i = 0; i < results; i++) {
+        const resultContainer = document.createElement('div')
+        resultContainer.classList.add('result-container')
+        resultContainer.id = `result-${i}`
+
+        const imageElement = document.createElement('img')
+        imageElement.src = meals[i].strMealThumb
+        imageElement.alt = 'recipe image'
+        imageElement.classList.add('recipe-image')
+
+        const detailsDiv = document.createElement('div')
+        detailsDiv.classList.add('details')
+
+        const leftDiv = document.createElement('div')
+        leftDiv.classList.add('left')
+
+        const h3Element = document.createElement('h3')
+        h3Element.textContent = meals[i].strMeal
+
+        const pElement = document.createElement('p')
+        pElement.textContent = meals[i].strArea
+
+        leftDiv.appendChild(h3Element)
+        if (meals[i].strArea != 'Unknown') {
+            leftDiv.appendChild(pElement)
+        }
+
+        const rightDiv = document.createElement('div')
+        rightDiv.classList.add('right')
+
+        const heartImageElement = document.createElement('img');
+        heartImageElement.classList.add('heart');
+        heartImageElement.src = '../images/profile_image.jpg';
+        heartImageElement.alt = 'heart image';
+
+        rightDiv.appendChild(heartImageElement);
+
+        detailsDiv.appendChild(leftDiv);
+        // detailsDiv.appendChild(rightDiv);
+        
+        resultContainer.appendChild(imageElement)
+        resultContainer.appendChild(detailsDiv)
+
+        resultsContainer.appendChild(resultContainer)
+    }
+}
