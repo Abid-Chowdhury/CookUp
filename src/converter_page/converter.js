1a51e3ab11fa9e95b33654234b233b98573ad01e
Abid-Chowdhury <mr.abid2004@gmail.com>
2023-08-29 18:37:17 -0400

    Updated converted page

---
+++ src/converter_page/converter.js
@@ -0,0 +1,15 @@
+if (first == cups) {
+    if (second == cups) {
+        return cups*1
+    } else if (second == tsp) {
+        return cups*1.2
+    }
+}
+
+if (first == tsbp) {
+    if (second == cups) {
+        return cups*1
+    } else if (second == tsp) {
+        return cups*1.2
+    }
+}
